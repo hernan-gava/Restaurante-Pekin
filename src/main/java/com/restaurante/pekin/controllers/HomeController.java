023dd1e05e83beee70ce7689d3e5fd407aaa5187
hernan-gava <de.software1524@gmail.com>
2022-03-22 12:54:34 -0600

    Cargando las nuevas vistas para los roles

--- src/main/java/com/restaurante/pekin/controllers/HomeController.java
+++ src/main/java/com/restaurante/pekin/controllers/HomeController.java
@@ -10,16 +10,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-   private static final String PAGINA_PRINCIPAL = "/index";
-   private static final String DETALLE_MENU = "/public-menu";
+	private static final String PAGINA_PRINCIPAL = "/index";
+	private static final String LOGIN = "/login-page";
+	private static final String DETALLE_MENU = "/public-menu";
+	private static final String Alta_Cliente = "/AltaCliente";
+	private static final String Alta_Role = "/AltaRole";
+	private static final String Alta_Cliente_Role = "/ClienteRole";
 
-   @GetMapping(value = PAGINA_PRINCIPAL)
-   public String paginaPrincipal() {
-      return "index";
-   }
+	@GetMapping(value = PAGINA_PRINCIPAL)
+	public String paginaPrincipal() {
+		return "index";
+	}
 
-   @GetMapping(value = DETALLE_MENU)
-   public String altaPublicacion() {
-      return "Administrador-Page/PublicacionAltaProductos";
-   }
+	@GetMapping(value = DETALLE_MENU)
+	public String altaPublicacion() {
+		return "Administrador-Page/PublicacionAltaProductos";
+	}
+
+	@GetMapping(value = LOGIN)
+	public String loginAdmin() {
+		return "Administrador-Page/Login-page";
+	}
+
+	@GetMapping(value = Alta_Cliente)
+	public String AltaCliente() {
+		return "Administrador-Page/AltaCliente";
+	}
+
+	@GetMapping(value = Alta_Role)
+	public String AltaRole() {
+		return "Administrador-Page/AltaRoles";
+	}
+
+	@GetMapping(value = Alta_Cliente_Role)
+	public String AltaClienteRole() {
+		return "Administrador-Page/ClienteRole";
+	}
 }
